295ff3e4bd3742658e8e12f48ca17d66207d9413
batariloa <batariloa@icloud.com>
2023-02-03 14:33:45 +0100

    Login hook refactored

--- blog-app/src/components/Login.js
+++ blog-app/src/components/Login.js
@@ -6,11 +6,15 @@ import "./css/Login.css";
 
 export function Login() {
   const navigate = useNavigate();
-  const [handleLogin, isLoading, error] = useLogin();
+  const { login, error } = useLogin();
 
   const emailRef = useRef();
   const passwordRef = useRef();
-  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(emailRef.current.value, passwordRef.current.value);
+  };
 
   //handle error in login request
   useEffect(() => {
@@ -22,7 +26,7 @@ export function Login() {
 
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form" onSubmit={handleLogin} autoComplete="on">
+      <form className="Auth-form" onSubmit={handleSubmit} autoComplete="on">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="form-group mt-3">
--- blog-app/src/hooks/useLogin.js
+++ blog-app/src/hooks/useLogin.js
@@ -1,47 +1,56 @@
 import { useState, useEffect, useRef } from "react";
+import { useAuthContext } from "./useAuthContext";
 import axiosClient from "../http/axios";
 import { url } from "../global/variables";
 
-export function useLogin() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const controller = useRef(new AbortController());
+export const useLogin = () => {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState();
 
+  const { dispatch } = useAuthContext();
+  const abortController = useRef(new AbortController());
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    abortController.current = new AbortController();
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    try {
+      const res = await axiosClient.post(
+        url + "/auth/login",
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          withCredentials: true,
+          signal: abortController.current.signal,
+        }
+      );
+      dispatch({ type: "LOGIN", payload: res.data });
+      localStorage.setItem("user", JSON.stringify(res.data));
+    } catch (err) {
+      if (err.name === "CanceledError") {
+        console.log("Canceled login.");
+      } else {
+        console.log("some other error", err);
+        setError("Incorrect credentials.");
+      }
+    }
+  };
+
+  //Cancel login on unmount
   useEffect(() => {
     return () => {
-      controller.current.abort();
+      abortController.current.abort();
     };
   }, []);
 
-  async function handleLogin(event) {
-    event.preventDefault();
-    setIsLoading(true);
-    setError(null);
-    const localController = new AbortController();
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-    try {
-      const loginRequest = await axiosClient.post(url + "/auth/login", {
-        // Your login data
-      }, {
-        signal: localController.signal,
-      });
-    
-      // Handle successful login
-      setIsLoading(false);
-      setError(null);
-    } catch (error) {
-      if (error.name === "AbortError") {
-        console.log("Aborted");
-        // Handle aborted request
-      } else {
-        console.log("Error:", error);
-        // Handle failed login
-        setIsLoading(false);
-        setError(error.message);
-      }
-    }
-  }
-
-  return [handleLogin, isLoading, error];
-}
+  return { login, error, isLoading };
+};
